b698458adc3971bf569fbb625c2b2763f8c2c1a8
quxiaoming <quxiaoming@szzt.com.cn>
2018-09-07 14:39:38 +0800

    [featrue] add destroy;

--- dev/index.js
+++ dev/index.js
@@ -11,10 +11,95 @@ new Vue({
     };
   },
   template: `
-    <div>
-        <div :class="['box', 'box1', {'active': show}]" v-ripple.test="{trigger: ['click']}">test</div>
-        <div :class="['box', 'box2', {'active': show}]" v-ripple.test="{trigger: ['click']}">test</div>
-        <div :class="['box', 'box3', {'active': show}]" v-ripple.test="{trigger: ['click']}">test</div>
-    </div>
+    <el-row>
+        <el-col :span="4">
+            <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-submenu index="1">
+                <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+                </template>
+                <el-menu-item-group>
+                <template slot="title">分组一</template>
+                <el-menu-item index="1-1" v-ripple.mouseenter>选项1</el-menu-item>
+                <el-menu-item index="1-2" v-ripple>选项2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="分组2">
+                <el-menu-item index="1-3" v-ripple>选项3</el-menu-item>
+                </el-menu-item-group>
+                <el-submenu index="1-4">
+                <template slot="title" v-ripple>选项4</template>
+                <el-menu-item index="1-4-1" v-ripple>选项1</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+            <el-menu-item index="2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">导航二</span>
+            </el-menu-item>
+            <el-menu-item index="3" disabled>
+                <i class="el-icon-document"></i>
+                <span slot="title">导航三</span>
+            </el-menu-item>
+            <el-menu-item index="4">
+                <i class="el-icon-setting"></i>
+                <span slot="title">导航四</span>
+            </el-menu-item>
+            </el-menu>
+        </el-col>
+        <el-col :span="20">
+            <el-row>
+                <el-button v-ripple>默认按钮</el-button>
+                <el-button v-ripple.mouseenter type="primary">主要按钮</el-button>
+                <el-button v-ripple.mouseenter type="success">成功按钮</el-button>
+                <el-button v-ripple.mouseenter type="info">信息按钮</el-button>
+                <el-button v-ripple.mouseenter type="warning">警告按钮</el-button>
+                <el-button v-ripple.mouseenter type="danger">危险按钮</el-button>
+            </el-row>
+            
+            <el-row>
+                <el-button v-ripple plain>朴素按钮</el-button>
+                <el-button v-ripple type="primary" plain>主要按钮</el-button>
+                <el-button v-ripple type="success" plain>成功按钮</el-button>
+                <el-button v-ripple type="info" plain>信息按钮</el-button>
+                <el-button v-ripple type="warning" plain>警告按钮</el-button>
+                <el-button v-ripple type="danger" plain>危险按钮</el-button>
+            </el-row>
+            
+            <el-row>
+                <el-button v-ripple round>圆角按钮</el-button>
+                <el-button v-ripple type="primary" round>主要按钮</el-button>
+                <el-button v-ripple type="success" round>成功按钮</el-button>
+                <el-button v-ripple type="info" round>信息按钮</el-button>
+                <el-button v-ripple type="warning" round>警告按钮</el-button>
+                <el-button v-ripple type="danger" round>危险按钮</el-button>
+            </el-row>
+            
+            <el-row>
+                <el-button v-ripple icon="el-icon-search" circle></el-button>
+                <el-button v-ripple type="primary" icon="el-icon-edit" circle></el-button>
+                <el-button v-ripple type="success" icon="el-icon-check" circle></el-button>
+                <el-button v-ripple type="info" icon="el-icon-message" circle></el-button>
+                <el-button v-ripple type="warning" icon="el-icon-star-off" circle></el-button>
+                <el-button v-ripple type="danger" icon="el-icon-delete" circle></el-button>
+            </el-row>
+            <el-row>
+                <el-card class="box-card" v-ripple>
+                    <div slot="header" class="clearfix">
+                    <span>卡片名称</span>
+                    <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                    </div>
+                    <div v-for="o in 4" :key="o" class="text item">
+                    {{'列表内容 ' + o }}
+                    </div>
+                </el-card>
+            </el-row>
+            
+        </el-col>
+    </el-row>
     `
 });
--- src/index.js
+++ src/index.js
@@ -3,45 +3,59 @@ import "./style/index.css";
 
 const isProduction = process.env.NODE_ENV === "production";
 
+const eventList = ["click", "mouseup", "touchend", "mouseenter", "touchstart"];
+
 class Ripple {
   constructor(option = {}) {
     let { el } = option;
+    if (!el) {
+      return this.$log("el not find");
+    }
+
     this.$el = el;
     this.$state = "hide";
     this.$option = Object.assign(
       {
-        trigger: "click",
+        trigger: [],
         during: 0.3,
         debugger: !isProduction
       },
       option
     );
 
-    if (!el) {
-      return this.$log("el not find");
-    }
-
     this.addEvent();
   }
 
+  addEvent() {
+    const { trigger } = this.$option;
+    trigger.map(e => {
+      on(this.$el, e, this.show.bind(this));
+    });
+  }
+
+  removeEvent() {
+    const { trigger } = this.$option;
+    trigger.map(e => {
+      off(this.$el, e, this.show.bind(this));
+    });
+  }
+
   $log(...arg) {
-    if (this.$option.debugger) {
+    if (this.$option && this.$option.debugger) {
       console.log(...arg);
     }
   }
 
-  addEvent() {
-    const el = this.$el;
-    on(el, "click", this.show.bind(this));
-  }
-
-  show(e = { offsetX: "50%", offsetY: "50%" }) {
+  show(e = { layerX: "50%", layerY: "50%" }) {
     let { width, height } = this.$el.getBoundingClientRect();
-    let { offsetX, offsetY } = e;
+    this.$el.style.position = "relative";
+    this.$el.style.overflow = "hidden";
+    let { layerX, layerY } = e;
+    console.log(layerX, layerY);
     const radius = Math.max(width, height) / 10;
 
     if (this.$state !== "hide") return;
-    this.$log("show", offsetX, offsetY);
+    this.$log("show", layerX, layerY);
     this.$state = "show";
 
     let rippleEl = document.createElement("div");
@@ -54,8 +68,8 @@ class Ripple {
     rippleEl.style.height = height + "px";
     rippleAnimate.style.width = radius + "px";
     rippleAnimate.style.height = radius + "px";
-    rippleAnimate.style.top = offsetY + "px";
-    rippleAnimate.style.left = offsetX + "px";
+    rippleAnimate.style.top = isNaN(layerY) ? layerY : layerY + "px";
+    rippleAnimate.style.left = isNaN(layerX) ? layerX : layerX + "px";
 
     rippleEl.append(rippleAnimate);
     this.$el.append(rippleEl);
@@ -80,17 +94,21 @@ class Ripple {
 
   destroy() {
     this.$state = "destroyed";
+    this.removeEvent();
   }
 }
 
 function bind(el, binding) {
+  let { modifiers } = binding;
+  const trigger = Object.keys(modifiers).filter(e => eventList.includes(e));
   el.ripple = new Ripple({
-    el
+    el,
+    trigger: trigger.length ? trigger : ["mouseup"]
   });
 }
 
 function unbind(el) {
-  console.log(el);
+  el.ripple.destroy();
 }
 
 export const VRipple = {
